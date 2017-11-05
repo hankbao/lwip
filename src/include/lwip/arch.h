93f3709d746f48facd8ca130ce4701a1ee4641e6
Dirk Ziegelmeier <dirk@ziegelmeier.net>
2017-11-05 10:27:46 +0100

    The macros in ip6_addr.c are duplicated from ip4_addr.c, so move them to lwIPs portability layer ...didn't see that in the first place... # Conflicts: #	src/include/lwip/arch.h

--- src/include/lwip/arch.h
+++ src/include/lwip/arch.h
@@ -216,20 +216,20 @@ typedef int ssize_t;
 #endif
 
 #if LWIP_NO_CTYPE_H
-#define lwip_in_range(c, lo, up)  ((u8_t)(c) >= (lo) && (u8_t)(c) <= (up))
-#define lwip_isdigit(c)           lwip_in_range((c), '0', '9')
-#define lwip_isxdigit(c)          (lwip_isdigit(c) || lwip_in_range((c), 'a', 'f') || lwip_in_range((c), 'A', 'F'))
-#define lwip_islower(c)           lwip_in_range((c), 'a', 'z')
-#define lwip_isspace(c)           ((c) == ' ' || (c) == '\f' || (c) == '\n' || (c) == '\r' || (c) == '\t' || (c) == '\v')
+#define lwip_in_range(c, lo, up)  ((u8_t)c >= lo && (u8_t)c <= up)
+#define lwip_isdigit(c)           lwip_in_range(c, '0', '9')
+#define lwip_isxdigit(c)          (lwip_isdigit(c) || lwip_in_range(c, 'a', 'f') || lwip_in_range(c, 'A', 'F'))
+#define lwip_islower(c)           lwip_in_range(c, 'a', 'z')
+#define lwip_isspace(c)           (c == ' ' || c == '\f' || c == '\n' || c == '\r' || c == '\t' || c == '\v')
 #define lwip_isupper(c)           lwip_in_range((c), 'A', 'Z')
 #define lwip_tolower(c)           (lwip_isupper(c) ? (c) - 'A' + 'a' : c)
 #define lwip_toupper(c)           (lwip_islower(c) ? (c) - 'a' + 'A' : c)
 #else
 #include <ctype.h>
-#define lwip_isdigit(c)           isdigit((unsigned char)(c))
-#define lwip_isxdigit(c)          isxdigit((unsigned char)(c))
-#define lwip_islower(c)           islower((unsigned char)(c))
-#define lwip_isspace(c)           isspace((unsigned char)(c))
+#define lwip_isdigit(c)           isdigit(c)
+#define lwip_isxdigit(c)          isxdigit(c)
+#define lwip_islower(c)           islower(c)
+#define lwip_isspace(c)           isspace(c)
 #define lwip_isupper(c)           isupper((unsigned char)(c))
 #define lwip_tolower(c)           tolower((unsigned char)(c))
 #define lwip_toupper(c)           toupper((unsigned char)(c))
